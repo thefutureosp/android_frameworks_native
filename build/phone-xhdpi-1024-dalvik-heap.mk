940e42212cdbbd7d0728d8c85868d102979832fc
Kyler <mbqsniper@gmail.com>
2013-08-12 22:01:17 -0700

    Add correct XHDPI heap size

--- build/phone-xhdpi-1024-dalvik-heap.mk
+++ build/phone-xhdpi-1024-dalvik-heap.mk
@@ -19,7 +19,7 @@
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.heapstartsize=8m \
     dalvik.vm.heapgrowthlimit=96m \
-    dalvik.vm.heapsize=256m \
+    dalvik.vm.heapsize=384m \
     dalvik.vm.heaptargetutilization=0.75 \
     dalvik.vm.heapminfree=512k \
     dalvik.vm.heapmaxfree=8m
